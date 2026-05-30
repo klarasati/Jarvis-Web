db1e230c438f2f5c8a9c5eddca29c923208bfd43
klarasati <122666095+klarasati@users.noreply.github.com>
2026-05-30 13:36:23 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                llama-3.1-8b-instan, // INI ADALAH OTAK VERSI TERBARU YANG AKTIF
+                llama-3.1-8b-instant, // INI ADALAH OTAK VERSI TERBARU YANG AKTIF
                 messages: req.body.messages,
                 temperature: 0.7
             })
